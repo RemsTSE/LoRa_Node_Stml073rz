54ee6e9771dff93b189fd3934705048111bfb58a
RemsTSE <95709109+RemsTSE@users.noreply.github.com>
2023-07-03 11:50:11 +0400

    update scheduler

--- LORA_node/Core/Inc/routing_table.h
+++ LORA_node/Core/Inc/routing_table.h
@@ -3,40 +3,34 @@
  *
  *  Created on: 7 juin 2023
  *      Author: remil
- *      Description : Implementation of the routing table of a node under the form of a binary search tree, containing
- *      the neighbours and 2nd-hop neighbours, with key (SF, id)
+ *      Description : Implementation of the routing table
  */
 #include <stdbool.h>
 #ifndef ROUTING_TABLE_H
 #define ROUTING_TABLE_H
+#define MAX_ENTRIES 20
 
-// Define the struct for the 2nd hop neighbors
-typedef struct SecondHop {
-    int id;
-    int SF;
-    struct SecondHop* left;
-    struct SecondHop* right;
-} SecondHop;
+typedef struct {
+    int dest_node_id;
+    bool dominant_dest;
+    int next_hop_id;
+    int cost;
+    int sf;
+} routing_entry_t;
 
-// Define the struct for the first hop neighbors
-typedef struct Neighbor {
-    int id;
-    int SF;
-    struct Neighbor* left;
-    struct Neighbor* right;
-    SecondHop* second_hop;
-} Neighbor;
+typedef struct {
+	int current_node_id;
+    int num_entries;
+    routing_entry_t entries[MAX_ENTRIES];
+} routing_table_t;
 
-// Function prototypes
-Neighbor* create_neighbor_node(int id, int SF);
-SecondHop* create_second_hop_node(int id, int SF);
-Neighbor* insert_neighbor(Neighbor* root, int id, int SF);
-SecondHop* insert_second_hop(SecondHop* root, int id, int SF);
-Neighbor* search_neighbor(Neighbor* root, int id);
-bool search_second_hop(SecondHop* root, int id);
-Neighbor* remove_neighbor(Neighbor* root, int id);
-SecondHop* remove_second_hop(SecondHop* root, int id);
-Neighbor* create_example_routing_table(Neighbor* routing_table);
+
+
+void add_entry(routing_table_t* table, int dest_node_id, int next_hop_id, int cost, int sf);
+void remove_entry(routing_table_t* table, int dest_node_id);
+routing_entry_t* get_entry(routing_table_t* table, int dest_node_id);
+void list_dominant_nodes(routing_table_t* table);
+void print_routing_table(routing_table_t* table);
 
 
 #endif
--- LORA_node/Core/Inc/scheduler.h
+++ LORA_node/Core/Inc/scheduler.h
@@ -5,35 +5,69 @@
  *      Author: remil
  */
 
+
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
-
+#include "node.h"
+#include "routing_table.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 #define MAX_TRANSMISSIONS 10
-#define MAX_CHANNELS 10
-#define MAX_SLOTS 100
+#define MAX_CHANNELS 5
+#define MAX_SLOTS 20
+#define MAX_NODES 100
 
 // Transmission data structure
 typedef struct {
     char source;
     char destination;
-    int sf;
-} transmission_t;
+    int spreading_factor;
+} Transmission;
+
+
+typedef struct {
+    int channel_index;
+    int time_slot;
+    Transmission transmission;
+} ScheduledTransmission;
+
+typedef struct {
+    int time_slot_index;
+    Transmission transmission;
+} TimeSlot;
+
+typedef struct {
+    Transmission ** transmissions;
+    int size;
+} Channel;
+
 
 // Schedule data structure
 
 typedef struct {
-    transmission_t transmissions[MAX_TRANSMISSIONS];
-    int slots[MAX_CHANNELS][MAX_SLOTS];
-    int num_transmissions;
+    Channel *channels;
     int num_channels;
-} schedule_t;
+} Schedule;
 
 // Function prototypes
-void add_transmission(schedule_t* schedule, char source, char destination, int sf);
-void schedule_transmissions(schedule_t* schedule);
-int compare_transmissions(const void* a, const void* b);
-void sort_transmissions(schedule_t* schedule);
-
+void add_transmission(Transmission **transmissions, int *num_transmissions, char source, char destination, int spreading_factor);
+int num_time_slots(int sf);
+Transmission* create_transmissions_from_routing_table(routing_table_t *table, int *num_transmissions);
+int compare_transmissions(const void *a, const void *b);
+int find_minimal_spreading_factor(Transmission *transmissions, int num_transmissions);
+void sort_transmissions_by_spreading_factor(Transmission *transmissions, int num_transmissions);
+void create_channels_list(Transmission *transmissions, int num_transmissions, int current_node_id, int *known_dominants, int num_known_dominants, Channel **channels_list, int *num_channels);
+bool is_id_in_known_dominants(int *known_dominants, int num_known_dominants, int id);
+int get_index_in_known_dominants(int known_dominants[], int num_known_dominants, int id);
+void cleanup_transmissions(Transmission **transmissions, int *num_transmissions);
+void cleanup_channels(Channel **channels_list, int num_channels);
+void cleanup_known_dominant_nodes(int **known_dominant_nodes);
+void schedule_transmissions(Channel *channels_list, int num_channels, ScheduledTransmission **scheduled_transmissions, int *num_scheduled_transmissions);
+//void cleanup_schedule(ScheduleEntry **schedule, int num_channels);
+bool is_duplicate(ScheduledTransmission *primary_scheduled_transmissions, int num_primary_scheduled_transmissions, ScheduledTransmission transmission);
+void merge_schedules(ScheduledTransmission *primary_scheduled_transmissions, int *num_primary_scheduled_transmissions,
+                     ScheduledTransmission *secondary_scheduled_transmissions, int num_secondary_scheduled_transmissions);
 
 
 
